e2e9c64a8e9b1402f0330f68d5586fae66c154a5
Elziekio <anthony.deamer@hotmail.com>
2021-09-04 17:30:50 +1000

    Adding the get and put requests to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,22 +3,65 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/api/tags', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try{
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/api/tags/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+    try {
+      const tagData = await Tag.findByPk(re.params.id, {
+        include: [{ model: Product }],
+      });
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag found with that id!' });
+        return;
+      }
+      res.json(200).json(tagData);
+    }catch (err) {
+      res.status(500).json(err);
+   }
 });
 
-router.post('/', (req, res) => {
+router.post('/api/tags', async (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then((newTag) => {
+    res.json(newTag);
+  })
+  .catch ((err) => {
+    res.json(err);
+  });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/api/tags/:id', async (req, res) => {
   // update a tag's name by its `id` value
+    try {
+      const tagData = await Tag.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!tagData[0]) {
+        res.status(404).json({ message: 'No tag with this id!' });
+        return;
+      }
+      req.status(200).json(tagData);
+    }catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.delete('/:id', (req, res) => {
