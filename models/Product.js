a1197edf9f314a7c04d7769a03aaba579f40799f
Elziekio <anthony.deamer@hotmail.com>
2021-09-04 14:05:55 +1000

    Updating models folder

--- models/Product.js
+++ models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    id: {
+    product_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: 'ProductTag',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: 'ProductTag',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
