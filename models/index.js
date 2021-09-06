46f5ccf6506c3465fe500e58179bf99ab3d0a562
Elziekio <anthony.deamer@hotmail.com>
2021-09-06 19:00:44 +1000

    Uodate routes

--- models/index.js
+++ models/index.js
@@ -6,25 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id',
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id',
+  // onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'ProductTag',
-  onDelete: 'CASCADE',
+  foreignKey: "product_id",
+  // onDelete: 'CASCADE',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'ProductTag',
-  onDelete: 'CASCADE',
+  foreignKey: "tag_id",
+  // onDelete: 'CASCADE',
 });
 
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try{
-    const categoryData = await Product.findAll({
+    const categoryData = await Category.findAll({
       include: [{ model: Product, attributes: ['product_name'] }],
     });
     res.status(200).json(categoryData);
